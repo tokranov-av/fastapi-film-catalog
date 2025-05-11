b86b56f47eb2ddb672bf9a3d4e09465de9636e6a
Artur Tokranov <a.tokranov@bars-open.ru>
2025-05-11 13:28:27 +0300

    Добавил возможность добавления нового фильма через POST запрос
    1. Добавил новое представление, которое принимает POST запрос с параметрами фильма в форме (Form Data);
    2. Из представления возвращается новый экземпляр фильма, составленного из принятых на вход данных.

--- film-catalog/api/api_v1/film_catalog_urls/views.py
+++ film-catalog/api/api_v1/film_catalog_urls/views.py
@@ -1,12 +1,17 @@
+from datetime import datetime
+import random
 from typing import Annotated
+
+from annotated_types import MinLen, Gt, Lt
 
 from fastapi import (
     Depends,
     APIRouter,
+    Form,
+    status,
 )
 
 from schemas.film import Film
-
 from .dependencies import prefetch_film
 from .crud import FILMS
 
@@ -22,6 +27,28 @@ router = APIRouter(
 )
 def get_list_of_films():
     return FILMS
+
+
+@router.post(
+    path="/",
+    response_model=Film,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_film(
+    name: Annotated[str, MinLen(1), Form()],
+    description: Annotated[str, Form()],
+    production_year: Annotated[int, Gt(1900), Lt(datetime.now().year + 1), Form()],
+    country: Annotated[str, MinLen(3), Form()],
+    genre: Annotated[str, MinLen(1), Form()],
+):
+    return Film(
+        id=random.randint(1, 1000),
+        name=name,
+        description=description,
+        production_year=production_year,
+        country=country,
+        genre=genre,
+    )
 
 
 @router.get(path="/{movie_id}/")
