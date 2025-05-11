c8f68103196f88e3db5bb5b59e8025bfeabb46a2
Artur Tokranov <a.tokranov@bars-open.ru>
2025-05-11 16:34:48 +0300

    Изменил способ передачи данных от клиента при создании фильма,- вместо Form используется формат JSON
    1. Объявил новую схему FilmCreate, которая включает все поля кроме поля id;
    2. Обновил представление для создания фильма,- данные принимаются в формате JSON вместо Form;
    3. В представлении создается новый экземпляр Film на основе полученных данных, id - генерируется.

--- film-catalog/schemas/film.py
+++ film-catalog/schemas/film.py
@@ -1,10 +1,14 @@
-from pydantic import BaseModel
+from datetime import datetime
+from typing import Annotated
+
+from annotated_types import MaxLen, MinLen, Gt, Lt
+
+from pydantic import BaseModel, Field
 
 
 class FilmBase(BaseModel):
     """Базовая модель фильма."""
 
-    id: int
     name: str
     description: str
     production_year: int
@@ -12,5 +16,17 @@ class FilmBase(BaseModel):
     genre: str
 
 
+class FilmCreate(FilmBase):
+    """Модель для создания фильма."""
+
+    name: Annotated[str, MinLen(1)]
+    description: Annotated[str, MaxLen(1000)]
+    production_year: Annotated[int, Gt(1900), Lt(datetime.now().year + 1)]
+    country: Annotated[str, MinLen(3)]
+    genre: Annotated[str, MinLen(1)]
+
+
 class Film(FilmBase):
     """Модель фильма."""
+
+    id: Annotated[int, Gt(0)]
