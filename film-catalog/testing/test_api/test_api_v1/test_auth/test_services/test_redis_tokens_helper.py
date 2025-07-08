64b4e209f5c86f197e251ff897f5a173f443f6b9
Artur Tokranov <a.tokranov@bars-open.ru>
2025-07-08 16:48:22 +0300

    Обновите тесты для помощника RedisTokensHelper

--- film-catalog/testing/test_api/test_api_v1/test_auth/test_services/test_redis_tokens_helper.py
+++ film-catalog/testing/test_api/test_api_v1/test_auth/test_services/test_redis_tokens_helper.py
@@ -5,11 +5,8 @@ from api.api_v1.auth.services import redis_tokens
 
 class RedisTokensHelperTestCase(TestCase):
     def test_generate_and_save_token(self) -> None:
-        expected_exists = True
-
         new_token = redis_tokens.generate_and_save_token()
 
-        self.assertEqual(
-            expected_exists,
+        self.assertTrue(
             redis_tokens.token_exists(new_token),
         )
