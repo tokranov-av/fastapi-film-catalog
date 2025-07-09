377770197669da77236723c9d011e8c74767fe19
Artur Tokranov <a.tokranov@bars-open.ru>
2025-07-09 22:30:02 +0300

    Добавил тесты обновления и частичного обновления для хранилища FilmStorage с использованием setUp и tearDown

--- film-catalog/testing/test_api/test_api_v1/test_film_catalog_urls/test_crud.py
+++ film-catalog/testing/test_api/test_api_v1/test_film_catalog_urls/test_crud.py
@@ -1,17 +1,70 @@
 import random
+import string
+from datetime import datetime
 from unittest import TestCase
 
+from api.api_v1.film_catalog_urls.crud import storage
+from core.config import TIME_ZONE
+from schemas.film import (
+    Film,
+    FilmCreate,
+    FilmPartialUpdate,
+    FilmUpdate,
+)
 
-def add_two_numbers(number1: int, number2: int) -> int:
-    return number1 + number2
 
+class FilmStorageUpdateTestCase(TestCase):
+    def setUp(self) -> None:
+        self.film = self.create_film()
+        self.expected_description = "Another description"
+        self.expected_genre = "Another genre"
 
-class AddTwoNumbersTestCase(TestCase):
-    def test_add_two_numbers(self) -> None:
-        number1 = random.randint(1, 100)  # noqa: S311
-        number2 = random.randint(1, 100)  # noqa: S311
-        expected = number1 + number2
+    def tearDown(self) -> None:
+        storage.delete(self.film)
 
-        result = add_two_numbers(number1, number2)
+    @classmethod
+    def create_film(cls) -> Film:
+        film_create = FilmCreate(
+            name="Some name",
+            description="Some description",
+            production_year=datetime.now(tz=TIME_ZONE).year,
+            country="Some country",
+            genre="Some genre",
+            slug="".join(
+                random.choices(  # noqa: S311
+                    string.ascii_letters,
+                    k=8,
+                ),
+            ),
+        )
 
-        self.assertEqual(expected, result)
+        return storage.create(film_create)
+
+    def test_update(self) -> None:
+        film_update = FilmUpdate(**self.film.model_dump())
+        film_update.description = self.expected_description
+        film_update.genre = self.expected_genre
+
+        updated_film = storage.update(
+            film=self.film,
+            film_in=film_update,
+        )
+
+        self.assertEqual(self.expected_description, updated_film.description)
+        self.assertEqual(self.expected_genre, updated_film.genre)
+
+    def test_partial_update(self) -> None:
+        film_partial_update = FilmPartialUpdate(
+            description=self.expected_description,
+            genre=self.expected_genre,
+        )
+        expected_name = self.film.name
+
+        partial_updated_film = storage.update_partial(
+            film=self.film,
+            film_in=film_partial_update,
+        )
+
+        self.assertEqual(self.expected_description, partial_updated_film.description)
+        self.assertEqual(self.expected_genre, partial_updated_film.genre)
+        self.assertEqual(expected_name, partial_updated_film.name)
