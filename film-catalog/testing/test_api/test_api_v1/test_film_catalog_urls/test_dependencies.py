ae4686041447c2315d6b1d54ae2e5d3cae5619fd
Artur Tokranov <a.tokranov@bars-open.ru>
2025-07-11 14:22:42 +0300

    Добавил тест проверки того, что все названия методов из UNSAFE_METHODS в верхнем регистре, и объединил тесты UNSAFE_METHODS в один класс

--- film-catalog/testing/test_api/test_api_v1/test_film_catalog_urls/test_dependencies.py
+++ film-catalog/testing/test_api/test_api_v1/test_film_catalog_urls/test_dependencies.py
@@ -1,11 +1,15 @@
 from api.api_v1.film_catalog_urls.dependencies import UNSAFE_METHODS
 
 
-def test_unsafe_methods_doesnt_contain_safe_methods() -> None:
-    safe_methods = {
-        "GET",
-        "HEAD",
-        "OPTIONS",
-    }
+class TestUnsafeMethods:
+    def test_doesnt_contain_safe_methods(self) -> None:
+        safe_methods = {
+            "GET",
+            "HEAD",
+            "OPTIONS",
+        }
 
-    assert not safe_methods & UNSAFE_METHODS
+        assert not safe_methods & UNSAFE_METHODS
+
+    def test_all_methods_are_upper(self) -> None:
+        assert all(method.isupper() for method in UNSAFE_METHODS)
